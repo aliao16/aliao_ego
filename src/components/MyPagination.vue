83c9e820efb2f1b23f291e769af101546072d5c0
aliao16 <481796550@qq.com>
2022-08-23 18:32:43 +0800

    后台管理系统

---
+++ src/components/MyPagination.vue
@@ -0,0 +1,49 @@
+<template>
+  <div style="text-align: center; margin: 20px">
+    <el-pagination background layout="total,prev, pager, next,jumper"
+      :total="total"
+      :page-size="pageSize"
+      :current-page="currentPage"
+      @current-change="changePage"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  // 准备父传子
+  props: {
+    // 总条数
+    total: {
+      type: Number,
+      default: 100,
+    },
+    // 页数
+    pageSize: {
+      type: Number,
+      default: 10,
+    },
+    // 当前页码
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+  },
+  // watch: {
+  //   currentPage(val) {
+  //     console.log("---val--", val);
+  //   },
+  // },
+  methods: {
+    // 子传父(Goods)
+    changePage(page) {
+      this.$emit("changePage", page);
+    },
+  },
+  created() {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="layout">
+      <!-- 左侧导航区域 -->
+      <MyMenu class="menu" :isCollapse='isCollapse'/>
+      <!-- 右侧内容区域 -->
+      <Content class="content" :class="{isActive:isCollapse}" @changeCollapse='changeCollapse' :isCollapse='isCollapse'/>
+  </div>
+</template>
+
+<script>
+// 页面分为左边菜单栏和右边内容
+import MyMenu from './Mymenu.vue'
+import Content from './Content.vue'
+export default {
+    components:{
+        MyMenu,
+        Content
+    },
+    data(){
+        return {
+            // 默认false
+            isCollapse:false,
+        }
+    },
+    methods:{
+        changeCollapse(){
+            this.isCollapse = !this.isCollapse;
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+.layout{
+    .menu{
+        background: #112f50;
+        // 左侧固定定位，不需要对layout flex
+        position: fixed;
+        top:0;
+        bottom:0;
+    }
+    .content{
+        margin-left: 200px;
+        transition: all 0.3s;
+    }
+    // 点击折叠后，isCollapse变true,右侧内容区域的margin-left变小
+    .isActive{
+        margin-left: 64px;
+    }
+}
+
+</style>
